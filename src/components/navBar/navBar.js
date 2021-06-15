bb9aab55fc07c7f68ba0b1b21e3fcf9a1005e0f3
Paula <43395223+PaulaE89@users.noreply.github.com>
2021-06-15 16:30:51 -0500

    add navBar

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,24 @@
+import './navBar.css'
+import hammy from '../../assets/hammy.png'
+import hammyTitle from '../../assets/hammyTitle.png'
+
+export const NavBar = () => {
+
+    const alarm = () => {
+        alert('there is no information at the moment')
+    }
+
+    return ( 
+    <nav className ="navBar"> 
+       <div className ="left-navBar"> 
+            <img src = {hammyTitle} className ='hammyTitle' alt='hammyimage'/>
+       </div>      
+     
+      <ul className="listItems" >               
+         <li onClick={alarm}>Category 1</li> 
+         <li onClick={alarm}>Category 2</li>
+         <li onClick={alarm}>Category 3</li>
+        </ul> 
+    </nav> )
+
+}
---
+++ src/home/home.js
@@ -0,0 +1,11 @@
+import { NavBar } from "../components/navBar/navBar"
+import './home.css'
+
+
+export const Home = () => {
+        return ( 
+            <div> <NavBar> </NavBar>                  
+            </div >
+            
+            )
+}
