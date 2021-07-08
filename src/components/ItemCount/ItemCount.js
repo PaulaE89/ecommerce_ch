8446cb15a4c879a3c3e32b169f8af92c996d73fa
paula-echeverri <paeb08@gmail.com>
2021-07-07 22:29:27 -0500

    add path cart

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -3,29 +3,29 @@ import React, { useState } from 'react'
 
 
 
-export const ItemCount = ({ value, stock ,onAdd,subtraccion,counter,disable,buyNow}) => {
-    // const [counter, setCounter] = useState(value);
-    // const [disable, setDisable] = useState(false);
+export const ItemCount = ({ value, stock ,onAdd}) => {
+    const [counter, setCounter] = useState(value);
+    const [disable, setDisable] = useState(false);
 
-    // const onAdd = () => {
-    //     if (counter === 10) {
-    //         setDisable(true)
-    //         alert(`there is only ${stock} in stock`)
-    //     } else {
-    //         setCounter((c) => c + 1);
-    //     }
-    // }
+    const addition = () => {
+        if (counter === 10) {
+            setDisable(true)
+            alert(`there is only ${stock} in stock`)
+        } else {
+            setCounter((c) => c + 1);
+        }
+    }
 
-    // const subtraccion = () => {
-    //     if (counter === 0) {
-    //         alert('It is zero')
-    //     } else if (counter === 10) {
-    //         setDisable(false)
-    //         setCounter((c) => c - 1)
-    //     } else {
-    //         setCounter((c) => c - 1)
-    //     }
-    // }
+    const subtraccion = () => {
+        if (counter === 0) {
+            alert('It is zero')
+        } else if (counter === 10) {
+            setDisable(false)
+            setCounter((c) => c - 1)
+        } else {
+            setCounter((c) => c - 1)
+        }
+    }
 
        
     const handleChange = (e) => {
@@ -41,10 +41,10 @@ export const ItemCount = ({ value, stock ,onAdd,subtraccion,counter,disable,buyN
                 <div className='btn_add_sub'>
                     <button onClick={subtraccion} className='btn_sign'>-</button>
                     <input value={counter} onChange={(e) => { handleChange(e) }} className='value_buy' ></input>
-                    <button onClick={onAdd} disabled={disable} className='btn_sign'>+</button>
+                    <button onClick={addition} disabled={disable} className='btn_sign'>+</button>
                 </div>
                 <div className='buy_btn'>
-               <button className='add_cart' onClick={buyNow}>ADD TO CART</button> 
+               <button className='add_cart' onClick={onAdd} value={counter}>ADD TO CART</button> 
                 </div>
             </div>
         </div>
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -7,47 +7,20 @@ import { NavLink } from 'react-router-dom'
 
 export const ItemDetail = (itemDetail) => {
 
-
-    let value = 0;
+    let value = 0
     let stock = '10'
-    
-    const [counter, setCounter] = useState(value);
-    const [disable, setDisable] = useState(false);
 
     const onAdd = () => {
-        if (counter === 10) {
-            setDisable(true)
-            alert(`there is only ${stock} in stock`)
-        } else {
-            setCounter((c) => c + 1);
-        }
-    }
 
-    const subtraccion = () => {
-        if (counter === 0) {
-            alert('It is zero')
-        } else if (counter === 10) {
-            setDisable(false)
-            setCounter((c) => c - 1)
-        } else {
-            setCounter((c) => c - 1)
-        }
-    }
-
-
-    //---------------
-
-    const buyNow = () => {
-      
-        if (counter === 0) {
+        let totalValue = document.getElementsByClassName('add_cart')[0]
+        totalValue = totalValue.value
+        if (totalValue === '0') {
             alert('you have to choose more than zero element')
-
         } else {
             let btndesaparecer = document.getElementsByClassName('btn_add_items')[0]
             btndesaparecer.style.display = 'none';
             let dis = document.getElementsByClassName('btn_confirm')[0]
             dis.style.display = 'block'
-
         }
     }
 
@@ -62,8 +35,8 @@ export const ItemDetail = (itemDetail) => {
                 <div className='sizeDetail'>Size : {itemDetail.itemDetail[0].size}</div>
                 <div className='descriptionDetail'> Description :  {itemDetail.itemDetail[0].description} </div>
                 <div className='priceDetail'>${itemDetail.itemDetail[0].price}</div>
-                <ItemCount value={value} stock={stock} onAdd={onAdd} subtraccion={subtraccion} counter={counter} disable={disable} buyNow={buyNow} />
-                <NavLink to='/cart' className='btn_confirm' counter={counter}><button className='btn_buy_now'>BUY NOW</button></NavLink>
+                <ItemCount value={value} stock={stock} onAdd={onAdd} />
+                <NavLink to='/cart' className='btn_confirm' ><button className='btn_buy_now'>BUY NOW</button></NavLink>
             </div>
         </div>
     )
